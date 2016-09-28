1f6146c9232044d712f6524feaf280cb6cb0cfc2
Giancarlo Ventura <sakya_stelios@hotmail.com>
2016-09-28 12:15:32 -0500

    update

--- index.js
+++ index.js
@@ -20,20 +20,5 @@ module.exports = {
 	        res = base[r].res;
 	      }
 		return res;
-	},
-
-  /**
-   * Unescape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
-   */
-  unescape: function(html) {
-    return String(html)
-      .replace(/&amp;/g, '&')
-      .replace(/&quot;/g, '"')
-      .replace(/&#39;/g, ''')
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>');
-  }
+	}
 };
