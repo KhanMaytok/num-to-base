4233266daf5535813dc9c797d48ba97742ef5bc1
Giancarlo Ventura <sakya_stelios@hotmail.com>
2016-09-28 17:10:47 -0500

    Added to base 10

--- index.js
+++ index.js
@@ -6,19 +6,31 @@
  * @return {String}
  */
 module.exports = {
-	toBase: function(num, b) {
-	    if(b == undefined){
-	    	b = 62;
-	    }
-	    var base='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	    var r = num % b;
-	    var res = base[r];
-	    var q = Math.floor(num/b);
-	    while (q) {
-	        r = q % b;
-	        q = Math.floor(q/b);
-	        res = base[r] + res;
-	      }
-		return res;
-	}
+    toBase: function(num, b) {
+        if (b == undefined) {
+            b = 62;
+        }
+        var base = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var r = num % b;
+        var res = base[r];
+        var q = Math.floor(num / b);
+        while (q) {
+            r = q % b;
+            q = Math.floor(q / b);
+            res = base[r] + res;
+        }
+        return res;
+    },
+    to10: function(num, b) {
+    	if (b == undefined) {
+            b = 62;
+        }
+        var base = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var limit = (num + '').length;
+        var res = base.indexOf(num[0]);
+        for (var i = 1; i < limit; i++) {
+            res = b * res + base.indexOf(num[i]);
+        }
+        return res;
+    }
 };
